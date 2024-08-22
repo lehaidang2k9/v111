7a82a91a8b5a59ebd4b898b71b5d8db624b5e209
lehaidang2k9 <146460634+lehaidang2k9@users.noreply.github.com>
2024-08-22 10:30:38 +0700

    inst.sh

--- inst.sh
+++ inst.sh
@@ -6,7 +6,7 @@ cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install w
 clear
 cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/etc/ && pkg install wget -y && wget https://github.com/lehaidang2k9/v111/raw/main/t1 && mv t1 motd
 clear
-cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v111/raw/main/l1 && chmod +x * && mv l1 login &&
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/lehaidang2k9/v111/raw/main/l1 && chmod +x * && mv l1 login &&
 clear
 echo "Đã cài đặt thành công"
 login
